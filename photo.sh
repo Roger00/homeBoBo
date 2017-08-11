8ca7ccb79f99d9c502a0dea563b66e99bf39179f
Roger <roger00.h@gmail.com>
2017-08-12 00:51:11 +0800

    Photo script is able to output to different folders
    - also backup old index.html

--- photo.sh
+++ photo.sh
@@ -5,6 +5,16 @@ timestamp() {
   date +"%Y-%m-%d_%H-%M-%S"
 }
 
+# select output folder
+if [ "${1}" = "pir" ]; then
+	out_folder="./pir_out/"
+else
+	out_folder="./out/"
+fi
+
+echo "Source argument: $1"
+echo "Output folder: ${out_folder}"
+
 f_name="photo.jpg"
 bak_f_name="$(timestamp).jpg"
 
@@ -12,22 +22,25 @@ echo "Switching to correct dir"
 cd /home/pi/scripts/
 
 echo "Taking photo...${bak_f_name}"
-raspistill -o "./out/${f_name}" -w 720 -h 720 -hf -vf 2>&1
+raspistill -o "${out_folder}${f_name}" -w 720 -h 720 -hf -vf 2>&1
 
 echo "Rotating photo..."
-convert -rotate 270 "./out/${f_name}" "./out/${f_name}"
+convert -rotate 270 "${out_folder}${f_name}" "${out_folder}${f_name}"
 
 echo "Adding water mark..."
-python waterMark.py "./out/${f_name}" "./out/${f_name}"
+python waterMark.py "${out_folder}${f_name}" "${out_folder}${f_name}"
 
 echo "Copy to backup dir"
-cp "./out/${f_name}" "./out/${bak_f_name}"
+cp "${out_folder}${f_name}" "${out_folder}${bak_f_name}"
 
-# echo "Copy to www dir"
-# cp "./out/${f_name}" "/var/www/html/out/${f_name}"
+# select output folder
+if [ "${1}" = "pir" ]; then
+	echo "Copy to www dir"
+	cp "${out_folder}${f_name}" "/var/www/html/pir_out/${f_name}"
 
-# echo "Touch www index file"
-# touch "/var/www/html/index.html"
-
-echo "Calling animated Gif script"
-/bin/bash ./animation.sh
+	echo "Touch www index file"
+	touch "/var/www/html/index.html"
+else
+	echo "Calling animated Gif script"
+	/bin/bash ./animation.sh
+fi
